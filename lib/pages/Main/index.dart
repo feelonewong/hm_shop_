89d6a78baf895c61c911f72c1f4fcb0cfdfdfc37
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2026-02-05 09:31:27 +0800

    feat(hm_shop): 完成底部导航布局

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -8,8 +8,44 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabList = [
+    {
+      'icon': 'lib/assets/tabs/home.png',
+      'active_icon': 'lib/assets/tabs/home_active.png',
+      "text": "首页",
+    },
+    {
+      'icon': 'lib/assets/tabs/category.png',
+      'active_icon': 'lib/assets/tabs/category_active.png',
+      "text": "分类",
+    },
+    {
+      'icon': 'lib/assets/tabs/cart.png',
+      'active_icon': 'lib/assets/tabs/cart_active.png',
+      "text": "购物车",
+    },
+    {
+      'icon': 'lib/assets/tabs/member.png',
+      'active_icon': 'lib/assets/tabs/member_active.png',
+      "text": "我的",
+    },
+  ];
+  List<BottomNavigationBarItem> _getTabBarWidgets() {
+    return List.generate(_tabList.length, (index) {
+      final item = _tabList[index];
+      return BottomNavigationBarItem(
+        icon: Image.asset(item['icon']!, width: 30, height: 30),
+        activeIcon: Image.asset(item['active_icon']!, width: 30, height: 30),
+        label: item['text'],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Main Page')));
+    return Scaffold(
+      body: Center(child: Text('Main Page')),
+      bottomNavigationBar: BottomNavigationBar(items: _getTabBarWidgets()),
+    );
   }
 }
